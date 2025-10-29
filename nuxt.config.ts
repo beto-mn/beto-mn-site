9159def9665ac3ae89dfe6252b669a4412107f21
Beto Najera <ing.betonajera@gmail.com>
2025-10-28 23:12:15 -0600

    🐛 fix: fix tailwindcss, add tailwindcss configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/storybook'],
+  modules: [
+    // '@nuxtjs/storybook'
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+  ],
 
-  // TypeScript configuration
   typescript: {
     typeCheck: true,
   },
